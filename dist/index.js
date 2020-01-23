d7c51cd6a30ab8e5ba30c2436bfa7f41590be969
Jesse Talavera-Greenberg <jesse@jesse.tg>
2020-01-22 22:57:53 -0500

    Whoops, forgot to build it

--- dist/index.js
+++ dist/index.js
@@ -57,22 +57,33 @@ const core = __webpack_require__(470);
 const _7z =  __webpack_require__(842);
 
 async function run() {
-  try { 
+  try {
     const source = core.getInput('pathSource');
     const target = core.getInput('pathTarget');
+    const args = core.getInput('args');
+
+    const _7zargs = ['a']; // "a"dd to archive
+
+    if (args != null) {
+      Array.prototype.push.apply(_7zargs, args.split(/ +/));
+      // Append all arguments to the arg array
+    }
+
+    _7zargs.push(target, source);
+    // Not a typo, the target comes before the source on the command line
 
     core.info("packing " + source + " into " + target);
     const err = await new Promise((resolve, _) => {
-      _7z.pack(source, target, function(e) {
-        resolve(e);
-      });
+      _7z.cmd(_7zargs, function(e) {
+          resolve(e);
+        });
     });
 
     if (err !== null) {
       core.setFailed("create 7z archive failed: " + err);
       return;
     }
-  } 
+  }
   catch (error) {
     core.setFailed(error.message);
   }
