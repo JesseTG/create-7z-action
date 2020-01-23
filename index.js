d29ca9a4e9f1b2569b8a72e5d98ddb692976f7e4
Jesse Talavera-Greenberg <jesse@jesse.tg>
2020-01-22 22:51:37 -0500

    Support arbitrary arguments added to the args array

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ async function run() {
   try { 
     const source = core.getInput('pathSource');
     const target = core.getInput('pathTarget');
+    const args = core.getInput('args');
+
+    const _7zargs = ['a'];
+
+    if (args != null) {
+
+      for (const a of args.split(/ +/)) {
+        _7zargs.push(a);
+      }
+    }
+
+    _7zargs.push(target, source);
 
     core.info("packing " + source + " into " + target);
     const err = await new Promise((resolve, _) => {
-      _7z.pack(source, target, function(e) {
-        resolve(e);
-      });
+      _7z.cmd(_7zargs, function(e) {
+          resolve(e);
+        });
     });
 
     if (err !== null) {
